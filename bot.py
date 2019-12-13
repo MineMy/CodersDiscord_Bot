e4380c61cb138a8fdebc80b214aca7f08e0e2884
Coder-Iro <doohee006@gmail.com>
2019-12-14 00:48:31 +0900

    Add emojis
    이모지기능만듬

--- bot.py
+++ bot.py
@@ -1,19 +1,31 @@
+import logging
 import os
+import sys
 
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+emojis: dict
+logger = logging.getLogger("discord")
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(logging.Formatter('[%(asctime)s] [%(levelname)s] %(name)s: %(message)s'))
+logger.addHandler(handler)
+bot = commands.Bot(command_prefix='!')
 
 
-@client.event
+@bot.event
 async def on_ready():
-    pass
+    global emojis
+    emojis = {x.name: x for x in bot.emojis}
 
 
-@client.event
-async def on_message(message: discord.Message):
-    if message.content == "안녕":
-        await message.channel.send("So What?")
+@bot.command(name="안녕")
+async def hello(context):
+    message: discord.Message = context.message
+    for emoji in ['c_', 'cpp', 'python', 'php', 'ruby', 'java', 'javascript', 'typescript', 'nodejs', 'html', 'css']:
+        await message.add_reaction(emojis[emoji])
+    await context.send("흠")
 
 
-client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])
